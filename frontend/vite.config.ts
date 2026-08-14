c39eb76e775715f53de858eac6ccf0d921a28d6e
dk5488 <divypandey104@gmail.com>
2026-08-15 02:56:21 +0530

    feat(project setup & configuration): add vite.config.ts

---
+++ frontend/vite.config.ts
@@ -0,0 +1,30 @@
+import tailwindcss from '@tailwindcss/vite';
+
+import { defineConfig } from 'vite';
+import path from 'path';
+
+import react from '@vitejs/plugin-react';
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@api': path.resolve(__dirname, './src/api'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://host.docker.internal:' + (process.env.BACKEND_PORT || '8000'),
+        changeOrigin: true,
+      },
+      '/content': {
+        target: 'http://host.docker.internal:' + (process.env.BACKEND_PORT || '8000'),
+        changeOrigin: true,
+      },
+    },
+  },
+});
