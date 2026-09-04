09fb31981ece314c6ee76c460f58eac6b4746e59
dk5488 <divypandey104@gmail.com>
2026-09-04 07:18:53 +0000

    feat(settings component): add UserPasswordEdit.tsx

---
+++ frontend/src/components/Settings/Auth/UserPasswordEdit.tsx
@@ -0,0 +1,92 @@
+'use client';
+
+import { Button } from '@/components/ui/button.tsx';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card.tsx';
+import { Input } from '@/components/ui/input.tsx';
+import { useContext } from 'react';
+import { StoreContext } from '@/store/storeContext.ts';
+import z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form.tsx';
+import { Spinner } from '@/components/ui/spinner.tsx';
+
+const formSchema = z
+  .object({
+    password: z.string().min(6, {
+      message: 'Password must be at least 6 characters.',
+    }),
+    confirm_password: z.string().min(6, {
+      message: 'Password must be at least 6 characters.',
+    }),
+  })
+  .refine((data) => data.password === data.confirm_password, {
+    message: "Passwords don't match",
+    path: ['confirm_password'],
+  });
+
+export function UserPasswordEdit() {
+  const store = useContext(StoreContext);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      password: '',
+      confirm_password: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const success = await store.updatePassword(values);
+    if (success) {
+      form.reset();
+    }
+    return success;
+  };
+
+  return (
+    <Card>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+          <CardHeader>
+            <CardTitle className="text-lg">Change Password</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-4">
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Password" autoComplete="new-password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirm_password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Confirm Password" autoComplete="new-password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+          <CardFooter className="flex flex-col gap-4">
+            <Button type="submit" className="w-full" disabled={form.formState.isSubmitting}>
+              {form.formState.isSubmitting && <Spinner />}
+              Change Password
+            </Button>
+          </CardFooter>
+        </form>
+      </Form>
+    </Card>
+  );
+}
