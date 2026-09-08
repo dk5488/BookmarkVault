3cb1a4930503e6e33bce8caeda50e3fe0574afab
dk5488 <divypandey104@gmail.com>
2026-09-08 07:19:26 +0000

    feat(table): add Pagination.tsx

---
+++ frontend/src/components/Table/Controls/Pagination.tsx
@@ -0,0 +1,87 @@
+import type { Table } from '@tanstack/react-table';
+import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight } from 'lucide-react';
+import { Button } from '../../ui/button.tsx';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../../ui/select.tsx';
+import { useLayoutEffect } from 'react';
+
+interface DataTablePaginationProps<TData> {
+  table: Table<TData>;
+}
+
+export function Pagination<TData>({ table }: DataTablePaginationProps<TData>) {
+  const pageIndex = table.getState().pagination.pageIndex;
+  useLayoutEffect(() => {
+    requestAnimationFrame(() => {
+      window.scrollTo({ top: 0, behavior: 'instant' });
+    });
+  }, [pageIndex]);
+
+  const rowCount = table.getFilteredRowModel().rows.length;
+
+  return (
+    <div className="flex flex-col gap-5 p-4">
+      <div className="text-muted-foreground flex-1/5 text-center text-sm">
+        {rowCount} item{rowCount !== 1 ? 's' : ''} total
+      </div>
+      <div className="flex flex-col items-center justify-between gap-4 @lg/main:flex-row">
+        <div className="flex items-center gap-2">
+          <div className="text-sm font-medium">Items per page</div>
+          <Select
+            value={table.getState().pagination.pageSize.toString()}
+            onValueChange={(value) => {
+              const newPageSize = Number(value);
+              table.setPageSize(newPageSize);
+            }}
+          >
+            <SelectTrigger>
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent side="top">
+              {[10, 25, 50, 100].map((pageSize) => (
+                <SelectItem key={pageSize} value={`${pageSize}`}>
+                  {pageSize}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="text-sm font-medium">
+            Page {pageIndex + 1} of {table.getPageCount()}
+          </div>
+          <Button
+            variant="outline"
+            className="@lg:flex"
+            onClick={() => table.firstPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <span className="sr-only">Go to first page</span>
+            <ChevronsLeft />
+          </Button>
+          <Button
+            variant="outline"
+            className=""
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <span className="sr-only">Go to previous page</span>
+            <ChevronLeft />
+          </Button>
+          <Button variant="outline" className="" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+            <span className="sr-only">Go to next page</span>
+            <ChevronRight />
+          </Button>
+          <Button
+            variant="outline"
+            className="lg:flex"
+            onClick={() => table.lastPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            <span className="sr-only">Go to last page</span>
+            <ChevronsRight />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
